63b585f0da0f68c02237b513c97c91c4abe73241
Mr_Hat <109857632+MrBaconHat@users.noreply.github.com>
2026-06-19 18:19:38 +0000

    Added JSONWatcher class for getting updated JSON for when the file is updated

--- nestwatch/watchers/json.py
+++ nestwatch/watchers/json.py
@@ -2,41 +2,50 @@ from ..core import FileSystemObserver
 
 import json
 
-from typing import Callable, Any
+from typing import Callable
 
 
 class JSONWatcher:
     def __init__(self, file_path):
         self.file_path = file_path
         self.observer = FileSystemObserver(self._on_modified, file_path)
-        self.callback: Callable | None = None
+        self.callback: Callable = None
         self.data = None
 
     async def _on_modified(self):
+        print("File modified")
         with open(self.file_path, "r") as f:
             data = json.load(f)
 
             # Compare the difference
-            diff = self._diff(data)
+            diff = self._diff(data, main=True)
             self.data = data
 
-            if self.callback and isinstance(self.callback, Callable):
+            if callable(self.callback):
                 await self.callback(diff)
 
-    def _diff(self, data) -> dict:
+    def _diff(self, data, main=True) -> dict:
         # Recursive function to find the difference between the old and new data
         # and then return the difference in the following format: e.g: {"old": {"key": "value"}, "new": {"key": "value"}}
         diff = {}
         if isinstance(data, dict):
             for key in data:
-                if key not in self.data:
+                if self.data is None or key not in self.data:
                     diff[key] = {"old": None, "new": data[key]}
-                    
-                elif data[key] != self.data.get(key):
-                    diff[key] = {"old": self.data.get(key), "new": data[key]}
 
                 elif isinstance(data[key], dict):
-                    diff[key] = self._diff(data[key])
+                    diff[key] = self._diff(data[key], main=False)
+                    
+                elif data[key] != self.data.get(key):
+                    diff[key] = self.data.get(key)
+
+
+        if main:
+            new_diff = {"old": {}, "new": {}}
+            for key in diff:
+                new_diff["old"][key] = self.data.get(key)
+                new_diff["new"][key] = data.get(key)
+                return new_diff
 
         return diff
 
@@ -46,8 +55,14 @@ class JSONWatcher:
         """
         self.callback = func
 
-    def start(self):
+    async def start(self):
         """
         Starts the watcher.
         """
-        self.observer.start()
+
+        # Up-to-date the cache before starting
+        with open(self.file_path, "r") as f:
+            self.data = json.load(f)
+
+        print("Starting watcher")
+        await self.observer.start()
