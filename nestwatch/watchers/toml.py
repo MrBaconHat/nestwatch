528907b7ba01f3f12a1325bcef5fa53e870028cd
Mr_Hat <109857632+MrBaconHat@users.noreply.github.com>
2026-06-30 16:12:56 +0000

    v0.0.10
    toml.py:
            1: Fixed the prebuilt TOML watcher, Turns out, we were giving it a Bytes object.

--- nestwatch/watchers/toml.py
+++ nestwatch/watchers/toml.py
@@ -1,10 +1,10 @@
 import aiofiles
 import tomllib
-    
+
 from .watcher import Watcher
 
 class TOMLWatcher(Watcher):
     async def _serialize(self):
         async with aiofiles.open(self.file_path, "rb") as f:
             content = await f.read()
-            return tomllib.load(content)
+            return tomllib.loads(content.decode("utf-8"))
